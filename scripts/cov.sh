06ae71c6641dec0a72d3f46eeb7b5c1ae9ebfe70
Balthazar Rouberol <br@imap.cc>
2022-04-27 16:30:58 +0200

    Fix coverage script that requires exported env vars

--- scripts/cov.sh
+++ scripts/cov.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+export RUSTFLAGS="-Cinstrument-coverage"
+export LLVM_PROFILE_FILE="bo-%p-%m.profraw"
 rm -rf *.profraw target/debug/coverage
-RUSTFLAGS="-Cinstrument-coverage" cargo build
-LLVM_PROFILE_FILE="bo-%p-%m.profraw" cargo test
+cargo build
+cargo test
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage
 open ./target/debug/coverage/index.html
 rm -rf *.profraw
