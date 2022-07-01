afbd82ab2629190411922db6dcf766912db773d8
brouberol <brouberol@users.noreply.github.com>
2022-07-01 09:27:38 +0000

    deploy: d1eefaaf93617cac1b8739bce098d8507bdde7c4

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["bo"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"bo/indexing/struct.LineNumber.html\" title=\"struct bo::indexing::LineNumber\">LineNumber</a>","synthetic":false,"types":["bo::indexing::LineNumber"]}];
+implementors["bo"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"bo/indexing/struct.LineNumber.html\" title=\"struct bo::indexing::LineNumber\">LineNumber</a>","synthetic":false,"types":["bo::indexing::LineNumber"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
-var N = null;var sourcesIndex = {};
+var sourcesIndex = {};
 sourcesIndex["bo"] = {"name":"","files":["commands.rs","config.rs","console.rs","document.rs","editor.rs","help.rs","history.rs","indexing.rs","main.rs","mode.rs","navigator.rs","row.rs","terminal.rs","utils.rs"]};
 createSourceSidebar();
